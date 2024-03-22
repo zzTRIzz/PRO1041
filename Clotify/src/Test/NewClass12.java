085c421c1cca0cb614e930282023660b2b0f7983
zzTRIzz <145739149+zzTRIzz@users.noreply.github.com>
2024-03-22 21:44:53 +0700

    Create NewClass12.java

---
+++ Clotify/src/Test/NewClass12.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test;
+
+/**
+ *
+ * @author Surface
+ */
+public class NewClass12 {
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
