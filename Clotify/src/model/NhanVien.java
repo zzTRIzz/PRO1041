08c183045f15a174a93baf03f9b4f52e8a61ec1f
zzTRIzz <145739149+zzTRIzz@users.noreply.github.com>
2024-03-22 17:08:52 +0700

    update

--- Clotify/src/model/NhanVien.java
+++ Clotify/src/model/NhanVien.java
@@ -9,22 +9,13 @@ package model;
  * @author Surface
  */
 public class NhanVien {
-<<<<<<< HEAD
     int maNV,vaiTro;
-=======
-    String maNV;
-    String  vaiTro;
->>>>>>> TuanPH45537
     String tenNV, ngaySinh,gioiTinh,diaChi,sdt,taiKhoan,matKhau;
 
     public NhanVien() {
     }
 
-<<<<<<< HEAD
     public NhanVien(int maNV, int vaiTro, String tenNV, String ngaySinh, String gioiTinh, String diaChi, String sdt, String taiKhoan, String matKhau) {
-=======
-    public NhanVien(String maNV, String vaiTro, String tenNV, String ngaySinh, String gioiTinh, String diaChi, String sdt, String taiKhoan, String matKhau) {
->>>>>>> TuanPH45537
         this.maNV = maNV;
         this.vaiTro = vaiTro;
         this.tenNV = tenNV;
@@ -36,7 +27,6 @@ public class NhanVien {
         this.matKhau = matKhau;
     }
 
-<<<<<<< HEAD
     public int getMaNV() {
         return maNV;
     }
@@ -107,93 +97,6 @@ public class NhanVien {
 
     public void setMatKhau(String matKhau) {
         this.matKhau = matKhau;
-=======
-    public String getMaNV() {
-        return maNV;
     }
-
-    public void setMaNV(String maNV) {
-        this.maNV = maNV;
-    }
-
-    public String getVaiTro() {
-        return vaiTro;
-    }
-
-    public void setVaiTro(String vaiTro) {
-        this.vaiTro = vaiTro;
-    }
-
-    public String getTenNV() {
-        return tenNV;
-    }
-
-    public void setTenNV(String tenNV) {
-        this.tenNV = tenNV;
-    }
-
-    public String getNgaySinh() {
-        return ngaySinh;
-    }
-
-    public void setNgaySinh(String ngaySinh) {
-        this.ngaySinh = ngaySinh;
-    }
-
-    public String getGioiTinh() {
-        return gioiTinh;
-    }
-
-    public void setGioiTinh(String gioiTinh) {
-        this.gioiTinh = gioiTinh;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
-
-    public String getSdt() {
-        return sdt;
-    }
-
-    public void setSdt(String sdt) {
-        this.sdt = sdt;
-    }
-
-    public String getTaiKhoan() {
-        return taiKhoan;
-    }
-
-    public void setTaiKhoan(String taiKhoan) {
-        this.taiKhoan = taiKhoan;
-    }
-
-    public String getMatKhau() {
-        return matKhau;
-    }
-
-    public void setMatKhau(String matKhau) {
-        this.matKhau = matKhau;
-    }
-
-    @Override
-    public String toString() {
-        return "NhanVien{" + "maNV=" + maNV + ", vaiTro=" + vaiTro + ", tenNV=" + tenNV + ", ngaySinh=" + ngaySinh + ", gioiTinh=" + gioiTinh + ", diaChi=" + diaChi + ", sdt=" + sdt + ", taiKhoan=" + taiKhoan + ", matKhau=" + matKhau + '}';
-    }
-
-  
-   
-      
-    public Object[]toDataRow(){
-        return new Object[]{
-            this.getMaNV(),this.getTenNV(),this.getNgaySinh(),this.getGioiTinh(),this.getDiaChi(),this.getSdt(),this.getTaiKhoan(),this.getMatKhau(),this.getVaiTro()
-        };
->>>>>>> TuanPH45537
-    }
-    
     
 }
