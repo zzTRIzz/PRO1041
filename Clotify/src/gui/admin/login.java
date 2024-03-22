0e9b61205b18f7b2d8ac5cdccd3c266bc020680f
zzTRIzz <145739149+zzTRIzz@users.noreply.github.com>
2024-03-22 17:53:38 +0700

    login UI and service

--- Clotify/src/gui/admin/login.java
+++ Clotify/src/gui/admin/login.java
@@ -1,8 +1,10 @@
+package gui.admin;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package gui.admin;
+
 
 import Service.TaiKhoanService;
 import com.formdev.flatlaf.FlatLightLaf;
