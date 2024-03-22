72f969ef7aacf82b9a9a8bc2bebaf8badca0d212
zzTRIzz <145739149+zzTRIzz@users.noreply.github.com>
2024-03-22 21:41:05 +0700

    Create triTest.java

---
+++ Clotify/src/Test/triTest.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class  {
+    public static void main(String[] args) {
+        System.out.println("sua luon cai này");
+        System.out.println("tri them van");
+    }
+}
