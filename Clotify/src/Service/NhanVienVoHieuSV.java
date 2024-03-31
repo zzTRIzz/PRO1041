ab1326ec8d2b569d0cc203f2cb9ee81fcde4c95a
zzTRIzz <145739149+zzTRIzz@users.noreply.github.com>
2024-03-31 22:28:41 +0700

    vo hieu

--- Clotify/src/Service/NhanVienVoHieuSV.java
+++ Clotify/src/Service/NhanVienVoHieuSV.java
@@ -9,42 +9,54 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import model.NhanVien;
 
 /**
  *
  * @author THE PC
  */
 public class NhanVienVoHieuSV {
-    public List<Object[]> getDisabledEmployees() {
-        List<Object[]> disabledEmployees = new ArrayList<>();
 
-        try (Connection connection = DBconnect.getConnection()) {
-            String sql = "SELECT maNV,vaiTro,tenNV,ngaySinh,gioiTinh,diaChi,sdt,taiKhoan,matKhau,email,trangThai FROM NhanVien Where trangThai=0";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
+    private List<NhanVien> disabledEmployees;
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+    private String sql = null;
 
-            while (resultSet.next()) {
-                Object[] rowData = new Object[11]; // S? l??ng tr??ng d? li?u c?a nhân viên
-                rowData[0] = resultSet.getString("maNV");
-                rowData[1] = resultSet.getString("vaiTro");
-                rowData[2] = resultSet.getString("tenNV");
-                rowData[3] = resultSet.getString("ngaySinh");
-                rowData[4] = resultSet.getString("gioiTinh");
-                rowData[5] = resultSet.getString("diaChi");
-                rowData[6] = resultSet.getString("sdt");
-                rowData[7] = resultSet.getString("taiKhoan");
-                rowData[9] = resultSet.getString("matKhau");
-                rowData[10] = resultSet.getString("email");
-                rowData[11] = resultSet.getString("trangThai");
-               
+    public List<NhanVien> getDisabledEmployees() {
+        disabledEmployees = new ArrayList<>();
 
-                disabledEmployees.add(rowData);
+        sql = "SELECT maNV,vaiTro,tenNV,ngaySinh,gioiTinh,diaChi,sdt,taiKhoan,matKhau,email,trangThai FROM NhanVien Where trangThai=0";
+
+        try {
+            con = DBconnect.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                NhanVien nv = new NhanVien(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11));
+                disabledEmployees.add(nv);
+
             }
+            return disabledEmployees;
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
 
-        return disabledEmployees;
     }
- 
+    
+    
+    
+    public int khoiPhucTrangThaiHoatDong(String ma) {
+        sql = "UPDATE NhanVien SET trangThai=1 WHERE  maNV=?";
+        try {
+            con = DBconnect.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, ma);
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 }
